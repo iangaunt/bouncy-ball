4c171e072d65c6ddde113023655c5ab5d06e027f
Ian G. <iangaunt999@gmail.com>
2024-05-21 13:53:52 -0700

    Edited website title

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,15 +1,10 @@
 import "../css/style.css";
 
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
 let x = 400;
 let y = 300;
-
-let prevMouseX = 0;
-let prevMouseY = 0;
-let mouseX = 0;
-let mouseY = 0;
 
 let vx = 5;
 let vy = 0;
@@ -17,9 +12,18 @@ let vy = 0;
 let ax = 0;
 let ay = 1;
 
+let prevMouseX = 0;
+let prevMouseY = 0;
+let mouseX = 0;
+let mouseY = 0;
+
+let retained = 0.8;
+
 let hue = 0;
 
 let grabbed = false;
+
+let hidden = "";
 
 function Main() {
     return (
@@ -30,8 +34,10 @@ function Main() {
             mouseX = e.clientX - 50;
             mouseY = e.clientY - 50;
         }}>
+            
             <div className="ball" onMouseUp={(e) => {
                 grabbed = false;
+
                 x = mouseX;
                 vx = mouseX - prevMouseX;
                 y = mouseY;
@@ -45,24 +51,42 @@ function Main() {
                 top: grabbed ? mouseY : y,
                 filter: "hue-rotate(" + hue + "deg)"
             }} ></div>
+            <div className="settings">
+                <button className="toggle" onClick={() => {
+                    hidden = hidden == "hidden" ? "" : "hidden"
+                }}>x</button>
+
+                <button className={hidden} onClick={() => {
+                    ay = ay == 0 ? 1 : 0;
+                }}>
+                    gravity: <span >{ay == 0 ? "off" : "on"}</span>
+                </button>
+                <button className={hidden}>
+                    retained E: <input type="number" onChange={(e) => {
+                        retained = parseFloat(e.target.value);
+                        if (retained < 0) retained = 0;
+                    }}></input>
+                </button>
+            </div>
         </div>
     )
 }
 
 let root = createRoot(document.getElementById("main"));
+
 setInterval(() => {
     if (!grabbed) x += vx;
     if (x < 0 || x > window.innerWidth - 100) {
         x = x < 50 ? 0 : window.innerWidth - 100;
         hue += 5;
-        vx *= -0.8;
+        vx *= -retained;
     }
 
     if (!grabbed) y += vy;
     if (y < 0 || y > window.innerHeight - 100) {
         y = y < 50 ? 0 : window.innerHeight - 100;
         hue += 5;
-        vy *= -0.8;
+        vy *= -retained;
     }
 
     if (!grabbed) vx += ax;
